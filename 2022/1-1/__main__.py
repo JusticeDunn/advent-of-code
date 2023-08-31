77870681fbf65e44f47bba695287378914925a7d
Justice Dunn <justicedunn@me.com>
2023-08-30 19:43:32 -0600

    Solve puzzle 1

--- 2022/1-1/__main__.py
+++ 2022/1-1/__main__.py
@@ -1,4 +1,16 @@
-import os
-
+# Read input file
 with open('input.txt') as f:
-    
+    lines = f.readlines()
+
+current_sum_of_calories = 0
+max_calories = 0
+
+for line in lines:
+    if line.strip() == '':
+        if current_sum_of_calories > max_calories:
+            max_calories = current_sum_of_calories
+        current_sum_of_calories = 0
+    else:
+        current_sum_of_calories += int(line)
+
+print('# of Calories it Contains: ', max_calories)
