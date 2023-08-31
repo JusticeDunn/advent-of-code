34bcf755a8561e06fc972d2d7721b7fe7a6ef958
Justice Dunn <justicedunn@me.com>
2023-08-30 19:48:33 -0600

    Complete day 1 puzzle 2

--- 2022/1-1/__main__.py
+++ 2022/1-1/__main__.py
@@ -4,13 +4,16 @@ with open('input.txt') as f:
 
 current_sum_of_calories = 0
 max_calories = 0
+calories = []
 
 for line in lines:
     if line.strip() == '':
         if current_sum_of_calories > max_calories:
             max_calories = current_sum_of_calories
+        calories.append(current_sum_of_calories)
         current_sum_of_calories = 0
     else:
         current_sum_of_calories += int(line)
 
-print('# of Calories it Contains: ', max_calories)
+calories.sort(reverse=True)
+print("# of Calories from top 3 elves: ", sum(calories[:3]))
